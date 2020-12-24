ee5004376aad6137bf08920dd01f933c4141ad08
ReymondEspinosa <reymond.espinosa06@gmail.com>
2020-12-24 13:54:20 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::post('/add-member', 'MembersController@create');
 
 Route::get('/admin', 'MembersController@indexAdmin');
 
-Route::get('/', 'MembersController@create');
+Route::get('/', 'MembersController@index');
 Route::post('/pick-member', 'MembersController@randomPicked');
 
 Route::get('/path-create', 'MembersController@pathCreate');
